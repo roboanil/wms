91b980509ebd53b19cfaa73e24f21a21d7de6c85
roboanil <anilrocksmamu007@gmail.com>
2018-04-24 21:44:33 +0530

    first commit

---
+++ js/info.js
@@ -0,0 +1,9 @@
+    $(function() {
+      $("#dialog").dialog({
+        position: {
+          my: "right bottom",
+          at: "right bottom",
+          of: window
+        }
+      });
+    });
